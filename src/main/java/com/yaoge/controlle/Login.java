dcc01977525a85260ae4f18abaa4de1e00643d1c
jingtinghuakaihualuo <hyy_19930607@163.com>
2018-01-13 20:16:18 +0800

    新增几个接口

--- src/main/java/com/yaoge/controlle/Login.java
+++ src/main/java/com/yaoge/controlle/Login.java
@@ -55,6 +55,26 @@ public class Login {
             retObj = Common.getSuccessResult();
             JSONObject result = new JSONObject();
             result.put("userid" , 666);
+            result.put("username" , "huang");
+            result.put("loginid" , loginid);
+            result.put("img" , "http://himg.bdimg.com/sys/portrait/item/0db2e58991e6838ae9a38e656e6a6f79692f.jpg");
+            result.put("background" , "http://e.hiphotos.baidu.com/image/h%3D300/sign=8d3a9ea62c7f9e2f6f351b082f31e962/500fd9f9d72a6059099ccd5a2334349b023bbae5.jpg");
+            result.put("deptid" , "01");
+            result.put("idcode" , "360428199305181019");
+            result.put("gender" , 0);
+            result.put("title" , "01");
+            result.put("educational" , "1");
+            result.put("mobilephone" , "13632505081");
+            result.put("birthday" , "05-18");
+            result.put("prov" , "41");
+            result.put("city" , "20");
+            result.put("area" , "11");
+            result.put("address" , "深圳市宝安区西乡固戍");
+            result.put("office" , "深圳市南山区");
+            result.put("familyphone" , "13818181818");
+            result.put("officephone" , "0755-12345678");
+            result.put("descri" , "帅的一比");
+            result.put("status" , "1");
             retObj.put("result" ,result);
         }else{
             retObj = Common.getFailResult();
